34859d0a7288b87673ac43e2246e99c75502f698
liorezra73 <53858691+liorezra73@users.noreply.github.com>
2020-03-28 19:11:35 +0300

    elastic

--- Data/elasticSearch.js
+++ Data/elasticSearch.js
@@ -1,15 +1,20 @@
 const { Client } = require("@elastic/elasticsearch");
 const client = new Client({ node: "http://localhost:9200" });
 const indexs = async () => {
-  const x = await client.index({
-    index: "game-of-thrones",
-    // type: '_doc', // uncomment this line if you are using Elasticsearch ≤ 6
-    body: {
-      character: "Daenerys Targaryen",
-      quote: "I am the blood of the dragon."
-    }
-  });
-  console.log(x)
+  try {
+    const x = await client.index({
+      index: "game-of-thrones",
+      // type: '_doc', // uncomment this line if you are using Elasticsearch ≤ 6
+      body: {
+        character: "Daenerys Targaryen",
+        quote: "I am the blood of the dragon."
+      }
+    });
+    console.log(x);
+  } catch (err) {
+    console.error(`${err.name}\n${err.message}`);
+    process.exit(1);
+  }
 };
 
 module.exports = indexs;
